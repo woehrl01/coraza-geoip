5b473282e308bbb9c8e67e15d6fc93ee494e2f78
Lukas Wöhrl <lukas.woehrl@plentymarkets.com>
2023-10-11 14:45:30 +0200

    wip

--- operators.go
+++ operators.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net"
 	"strconv"
+	"strings"
 
 	"github.com/corazawaf/coraza/v3/collection"
 	"github.com/corazawaf/coraza/v3/experimental/plugins/plugintypes"
@@ -14,40 +15,34 @@ import (
 
 type geo struct {
 	db 		 *geoip2.Reader
+	dbtype string
 }
 
 func newGeolookup(options plugintypes.OperatorOptions) (plugintypes.Operator, error) {
-	data := options.Arguments
+	data := strings.Split(options.Arguments, " ")
 
-	databaseFile := data
+	if len(data) < 2 {
+		return nil, fmt.Errorf("missing database file")
+	}
+
+	databaseFile := data[0]
 	if databaseFile == "" {
 		return nil, fmt.Errorf("missing database file")
 	}
+
+	dbtype := data[1]
 
 	db, err := geoip2.Open(databaseFile)
 	if err != nil {
 		return nil, err
 	}
 
-	return &geo{db: db}, nil
+	return &geo{db: db, dbtype: dbtype}, nil
 }
 
-func (o *geo) Evaluate(tx plugintypes.TransactionState, value string) bool {
-	ip := net.ParseIP(value)
+func (o *geo) ApplyVariablesCity(col collection.Map, ip net.IP) bool {
 	r, err := o.db.City(ip)
 	if err != nil {
-		return false
-	}
-
-	var col collection.Map
-	if c, ok := tx.Collection(variables.Geo).(collection.Map); !ok {
-		tx.DebugLogger().Error().Msg("collection in getset is not a map")
-		return false
-	} else {
-		col = c
-	}
-	if col == nil {
-		tx.DebugLogger().Error().Msg("collection in getset is nil")
 		return false
 	}
 
@@ -61,4 +56,44 @@ func (o *geo) Evaluate(tx plugintypes.TransactionState, value string) bool {
 	col.Set("longitude", []string{strconv.FormatFloat(r.Location.Longitude, 'f', 10, 64)})
 
 	return true
+}
+
+func (o *geo) ApplyVariablesCountry(col collection.Map, ip net.IP) bool {
+	r, err := o.db.Country(ip)
+	if err != nil {
+		return false
+	}
+
+	col.Set("country_code", []string{r.Country.IsoCode})
+	col.Set("country_name", []string{r.Country.Names["en"]})
+	col.Set("country_continent", []string{r.Continent.Names["en"]})
+
+	return true
+}
+
+
+func (o *geo) Evaluate(tx plugintypes.TransactionState, value string) bool {
+	ip := net.ParseIP(value)
+
+	var col collection.Map
+	if c, ok := tx.Collection(variables.Geo).(collection.Map); !ok {
+		tx.DebugLogger().Error().Msg("collection in getset is not a map")
+		return false
+	} else {
+		col = c
+	}
+	if col == nil {
+		tx.DebugLogger().Error().Msg("collection in getset is nil")
+		return false
+	}
+
+	switch(o.dbtype) {
+	case "city":
+		return o.ApplyVariablesCity(col, ip)
+	case "country":
+		return o.ApplyVariablesCountry(col, ip)
+	default:
+		tx.DebugLogger().Error().Msg("invalid database type")
+		return false
+	}
 }
